4546f2f53b192a8caf03fa307fa1543f15a14fa2
Oxidaner <18622412361@163.com>
2022-05-28 20:58:32 +0800

    5.28

--- vue-advance/01_src_分析脚手架/main.js
+++ vue-advance/01_src_分析脚手架/main.js
@@ -1,7 +1,10 @@
+// 该文件是整个项目的入口文件
 import Vue from 'vue' //这里引入的是残缺版的vue,是没有模版解析器是不能写template的 vue.runtime.esm.js
+//引入App组件,它是所有组建的父组件
 import App from './App.vue'
 // import Vue from 'vue/dist/vue';
 
+//关闭vue的生产提示
 Vue.config.productionTip = false;
 
 /*
@@ -15,11 +18,14 @@ Vue.config.productionTip = false;
 			render函数接收到的createElement函数去指定具体内容。
 */
 
+//创建vue对象
 new Vue({
   // template: `<App></App>`,
   // components: { App },
-  // render: h => h(App),
+
+  // render: h => h(App), 
   el:'#app',
+  //将App放到容器中
   render: h => h(App),
   //vue 传递帮你调render函数并传递了一个名为createElement的函数,这里的第一个参数代表h1元素，第二个参数是h1的内容
   // render:(createElement) => createElement('h1',"迟缓")
--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
+// 该文件是整个项目的入口文件
+import Vue from 'vue' 
+//引入App组件,它是所有组建的父组件
 import App from './App.vue'
 
+//关闭vue的生产提示
 Vue.config.productionTip = false
 
+//创建Vue实例对象 --- vm
 new Vue({
-  render: h => h(App),
+    //下面这行代码一会解释,完成了这个功能:将App组件放入容器中
+    render: h => h(App),
 }).$mount('#app')
+
+// new Vue({
+//     el:'#app',
+//     render: h => h(App),
+// })
