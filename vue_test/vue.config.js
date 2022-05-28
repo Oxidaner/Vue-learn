a2712d57b8014026a2b03438017402f3f5c9ef59
Oxidaner <18622412361@163.com>
2022-05-29 01:24:31 +0800

    5.29

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+    },
+  },
+  //关闭与语法警告
+  lintOnSave:false
+}
