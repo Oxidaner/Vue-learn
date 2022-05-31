0c290226cbdfc8ed5f797edde4c7417f719fd574
Oxidaner <18622412361@163.com>
2022-05-31 17:52:48 +0800

    5.31

--- vue_test/13_src_Todolist全局总线事件版本/components/Item.vue
+++ vue_test/13_src_Todolist全局总线事件版本/components/Item.vue
@@ -1,16 +1,12 @@
 <template>
   <li>
     <label>
-      <!--      这里勾选和取消勾选可以使用change和click作为事件处理-->
-      <input
-        type="checkbox"
-        :checked="todo.done"
-        @change="handleCheck(todo.id)"
-      />
+      <!--这里勾选和取消勾选可以使用change和click作为事件处理-->
+      <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
       <!--v-model数据的双向绑定，checkbox使用v-model来双向绑定其是否被勾选,也可以实现效果但不推荐(因为其实修改了props中的数据)-->
       <!--这里修改了从List修改过来的props,这里的不允许改是浅层次，就是如果props是一个对象则这个修改这个对象的某一个属性vue是放行的-->
       <!-- <input type="checkbox" v-model="todo.done"/>-->
-      <span>{{ todo.title }}</span>
+      <span>{{  todo.title }}</span>
     </label>
     <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
   </li>
@@ -20,19 +16,21 @@
 export default {
   name: "Item",
   //声明接收todo
-  props: ["todo", "checkTodo", "deleteTodo"],
-  methods: {
-    handleCheck(id) {
-      this.checkTodo(id);
+  props: ['todo'],
+  methods:{
+    handleCheck(id){
+      //事件总线
+      this.$bus.$emit('checkTodo',id);
     },
-    handleDelete(id) {
-      if (confirm(`确定删除编号为${id}的todo吗`)) {
+    handleDelete(id){
+      if(confirm(`确定删除编号为${id}的todo吗`)){
         // console.log(id);
-        this.deleteTodo(id);
+        //事件总线
+        this.$bus.$emit('deleteTodo',id);
       }
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -71,11 +69,11 @@ li:last-child {
   border-bottom: none;
 }
 
-li:hover {
+li:hover{
   background: #ddd;
 }
 
-li:hover button {
+li:hover button{
   display: block;
 }
 </style>
